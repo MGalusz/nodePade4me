6923371d34e03533b019826f5bc2770818e022ce
MGalusz <galuszka.mm@gmail.com>
2017-08-23 16:07:16 +0200

    global view of aplication

---
+++ notepad4Me/notepad4Me/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace notepad4Me
+{
+    public partial class frmMain : Form
+    {
+        string ourFileName;
+
+        string lastFindWorld;
+        bool lastFindDown;
+        bool lastFindMatchCase;
+
+        void doSave(string filename)
+        {
+            ourFileName = filename;
+            textBox.SaveFile(filename);
+        }
+
+        void doSaveAs()
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                doSave(saveFileDialog1.FileName);
+            }
+        }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox.Undo();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox.Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox.Paste();
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox.Clear();
+        }
+
+        bool checkChanges()
+        {
+            return true;
+        }
+
+        private void findToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           frmFind find = new frmFind();
+            find.Show(this);
+        }
+        
+
+        private void findNextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+             frmMain parent_form = (frmMain)Owner;
+             parent_form.DoFind(textFind.Text, rdoDown, chkmatch.Checked);
+        }
+
+        public void DoFind(string search, bool down, bool match_case)
+        {
+            lastFindWorld = search;
+            lastFindDown = down;
+            lastFindMatchCase = match_case;
+            if (down)
+            {
+                if (match_case)
+                {
+                    textBox.Find(search, textBox.SelectionStart + 1, RichTextBoxFinds.MatchCase);
+                }
+                else
+                {
+                    textBox.Find(search, textBox.SelectionStart + 1, RichTextBoxFinds.None);
+                }
+            }
+        }
+
+        private void replaceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox.SelectAll();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (checkChanges())
+            {
+
+            
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    textBox.LoadFile(openFileDialog1.FileName);
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ourFileName))
+            {
+                doSaveAs();
+            }
+            else
+            {
+                doSave(ourFileName);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ notepad4Me/notepad4Me/Form2.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace notepad4Me
+{
+    partial class frmFind
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFind = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textFind = new System.Windows.Forms.TextBox();
+            this.chkMatchCase = new System.Windows.Forms.CheckBox();
+            this.Direction = new System.Windows.Forms.GroupBox();
+            this.rdoUp = new System.Windows.Forms.RadioButton();
+            this.rdoDown = new System.Windows.Forms.RadioButton();
+            this.Direction.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(261, 12);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "Find Next";
+            this.btnFind.UseVisualStyleBackColor = true;
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Location = new System.Drawing.Point(261, 60);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(75, 25);
+            this.BtnCancel.TabIndex = 1;
+            this.BtnCancel.Text = "Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Fine What";
+            // 
+            // textFind
+            // 
+            this.textFind.Location = new System.Drawing.Point(74, 9);
+            this.textFind.Multiline = true;
+            this.textFind.Name = "textFind";
+            this.textFind.Size = new System.Drawing.Size(170, 38);
+            this.textFind.TabIndex = 3;
+            // 
+            // chkMatchCase
+            // 
+            this.chkMatchCase.AutoSize = true;
+            this.chkMatchCase.Location = new System.Drawing.Point(15, 97);
+            this.chkMatchCase.Name = "chkMatchCase";
+            this.chkMatchCase.Size = new System.Drawing.Size(83, 17);
+            this.chkMatchCase.TabIndex = 4;
+            this.chkMatchCase.Text = "Match Case";
+            this.chkMatchCase.UseVisualStyleBackColor = true;
+            // 
+            // Direction
+            // 
+            this.Direction.Controls.Add(this.rdoDown);
+            this.Direction.Controls.Add(this.rdoUp);
+            this.Direction.Location = new System.Drawing.Point(104, 85);
+            this.Direction.Name = "Direction";
+            this.Direction.Size = new System.Drawing.Size(126, 45);
+            this.Direction.TabIndex = 5;
+            this.Direction.TabStop = false;
+            this.Direction.Text = "Direction";
+            // 
+            // rdoUp
+            // 
+            this.rdoUp.AutoSize = true;
+            this.rdoUp.Location = new System.Drawing.Point(3, 16);
+            this.rdoUp.Name = "rdoUp";
+            this.rdoUp.Size = new System.Drawing.Size(39, 17);
+            this.rdoUp.TabIndex = 0;
+            this.rdoUp.TabStop = true;
+            this.rdoUp.Text = "Up";
+            this.rdoUp.UseVisualStyleBackColor = true;
+            // 
+            // rdoDown
+            // 
+            this.rdoDown.AutoSize = true;
+            this.rdoDown.Location = new System.Drawing.Point(67, 16);
+            this.rdoDown.Name = "rdoDown";
+            this.rdoDown.Size = new System.Drawing.Size(53, 17);
+            this.rdoDown.TabIndex = 1;
+            this.rdoDown.TabStop = true;
+            this.rdoDown.Text = "Down";
+            this.rdoDown.UseVisualStyleBackColor = true;
+            // 
+            // frmFind
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 257);
+            this.Controls.Add(this.Direction);
+            this.Controls.Add(this.chkMatchCase);
+            this.Controls.Add(this.textFind);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.btnFind);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmFind";
+            this.Text = "Fine";
+            this.Direction.ResumeLayout(false);
+            this.Direction.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textFind;
+        private System.Windows.Forms.CheckBox chkMatchCase;
+        private System.Windows.Forms.GroupBox Direction;
+        private System.Windows.Forms.RadioButton rdoDown;
+        private System.Windows.Forms.RadioButton rdoUp;
+    }
+}
